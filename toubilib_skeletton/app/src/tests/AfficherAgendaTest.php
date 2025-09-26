0ca43136a66a3cd9719c77c8b020a71e1ed23fcb
Kinnig Felicien <felicien.kinnig9@etu.univ-lorraine.fr>
2025-09-26 20:11:45 +0200

    classe de test fonctionnalité 5, 6, 7 (non testé)

---
+++ toubilib_skeletton/app/src/tests/AfficherAgendaTest.php
@@ -0,0 +1,31 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use toubilib\core\domain\entities\ServiceRendezVousInterface as ServiceRendezVousInterface;
+use DI\Container;
+
+class AfficherAgendaTest extends TestCase{
+
+    private ServiceRendezVousInterface $service;
+
+    public function setUp():void{
+        $container = new Container();
+        $services = require __DIR__ . '\..\..\config\services.php';
+
+        foreach($services as $id => $content){
+            $container->set($id, $content);
+        }
+
+        $this->service = $container->get(ServiceRendezVousInterface::class);
+    }
+
+    public function test_listerCrenaux(){
+        $agenda = $this->service->consulterAgenda('4305f5e9-be5a-4ccf-8792-7e07d7017363', new \DateTimeImmutable('2025-12-01 8:00:00'), new \DateTimeImmutable('2025-12-03 10:30:00'));
+        $this->assertEquals(true, is_array($res));
+
+        foreach($agenda as $rdv){
+            $this->AssertArrayHasKey('patient', $rdv);
+            $this->AssertArrayHasKey('motif_visite', $rdv);
+        }
+    }
+}
---
+++ toubilib_skeletton/app/src/tests/AnnulerRendezVousTest.php
@@ -0,0 +1,39 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use toubilib\core\domain\entities\ServiceRendezVousInterface as ServiceRendezVousInterface;
+use DI\Container;
+use toubilib\core\application\ports\api\dtos\InputRendezVousDTO as InputRendezVousDTO;
+
+class AnnulerRendezVousTest extends TestCase{
+
+    private ServiceRendezVousInterface $service;
+
+    public function setUp():void{
+        $container = new Container();
+        $services = require __DIR__ . '\..\..\config\services.php';
+
+        foreach($services as $id => $content){
+            $container->set($id, $content);
+        }
+
+        $this->service = $container->get(ServiceRendezVousInterface::class);
+    }
+
+    public function test_CreerRendezVous(){
+        $dto = new InputRendezVousDTO(
+            '4305f5e9-be5a-4ccf-8792-7e07d7017363',
+            'd975aca7-50c5-3d16-b211-cf7d302cba50',
+            new \DateTimeImmutable('tomorrow 11:00'),
+            30,
+            'scanner'
+        );
+        $rdvId = $this->service->creerRendezVous($dto);
+        $this->assertNotNull($rdvId);   
+        print($rdvId);
+        $this->service->annulerRendezVous($dto);
+        $rdvAnnuler = $this->service->findById($dto);
+        $this->assertSame(2, $rdvAnnuler->__get('statut'));
+        print($rdvAnnuler->__get('statut'));
+    }
+}
