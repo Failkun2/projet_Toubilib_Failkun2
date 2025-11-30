0013d72d7308f2e1f6775f83ae416033eee3cbda
Kinnig Felicien <felicien.kinnig9@etu.univ-lorraine.fr>
2025-11-30 18:24:53 +0100

    update(dto): patient
    correction du nom d'attribut

--- toubilib/app/src/application_core/application/ports/api/dtos/PatientDTO.php
+++ toubilib/app/src/application_core/application/ports/api/dtos/PatientDTO.php
@@ -7,14 +7,14 @@ class PatientDTO implements \JsonSerializable{
  
     private string $nom;
     private string $prenom;
-    private string $date_naissance;
+    private string $dateNaissance;
     private string $email;
     private string $telephone;
 
-    public function __construct(string $nom, string $prenom, string $date_naissance, string $email, string $telephone){
+    public function __construct(string $nom, string $prenom, string $dateNaissance, string $email, string $telephone){
         $this->nom = $nom;
         $this->prenom = $prenom;
-        $this->date_naissance = $date_naissance;
+        $this->dateNaissance = $dateNaissance;
         $this->email = $email;
         $this->telephone = $telephone;
     }
@@ -23,7 +23,7 @@ class PatientDTO implements \JsonSerializable{
         return[
             'nom' => $this->nom,
             'prenom' => $this->prenom,
-            'date_naissance' => $this->ville,
+            'dateNaissance' => $this->dateNaissance,
             'email' => $this->email,
             'telephone' => $this->telephone,
         ];
