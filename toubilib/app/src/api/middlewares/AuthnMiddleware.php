41dbb27629b11369219f989889caa762f00d3c40
Kinnig Felicien <felicien.kinnig9@etu.univ-lorraine.fr>
2025-11-30 18:37:51 +0100

    update(middleware): authn
    correction authnmiddleware

--- toubilib/app/src/api/middlewares/AuthnMiddleware.php
+++ toubilib/app/src/api/middlewares/AuthnMiddleware.php
@@ -2,10 +2,10 @@
 
 namespace toubilib\api\middlewares;
 
-use Psr\Server\MiddlewareInterface;
+use Psr\Http\Server\MiddlewareInterface;
 use Psr\Container\ContainerInterface;
 use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Server\RequestHanderInterface;
+use Psr\Http\Server\RequestHandlerInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use toubilib\api\provider\JWTAuthnProvider as JWTAuthnProvider;
 
@@ -14,10 +14,10 @@ class AuthnMiddleware implements MiddlewareInterface{
     private JWTAuthnProvider $authnProvider;
 
     public function __construct(JWTAuthnProvider $authnProvider){
-        $this->authnProvider = $authnProvider
+        $this->authnProvider = $authnProvider;
     }
     
-    public function __invoke(ServerRequestInterface $rq, RequestHanderInterface $rh) : Response{
+    public function __invoke(ServerRequestInterface $rq, RequestHandlerInterface $rh) : Response{
         $header = $rq->getHeaderLine('Authorization');
         if(!$header || !str_starts_with($header, 'Bearer ')){
             return new Response(401, ['Content-type' => 'application/json'], json_encode(['erreur' => 'Token manquant']));
