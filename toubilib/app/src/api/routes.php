8500520a843f54a1069ed186f690cc559bfe2019
Kinnig Felicien <felicien.kinnig9@etu.univ-lorraine.fr>
2025-11-30 18:27:53 +0100

    update(route): commentaires
    ajout de commentaires

--- toubilib/app/src/api/routes.php
+++ toubilib/app/src/api/routes.php
@@ -14,25 +14,29 @@ use toubilib\api\actions\AnnulerRendezVousAction as AnnulerRendezVousAction;
 use toubilib\api\actions\ConsulterAgendaAction as ConsulterAgendaAction;
 use toubilib\api\actions\SignInAction as SignInAction;
 use toubilib\api\actions\RefreshAction as RefreshAction;
+use toubilib\api\middlewares\AuthnMiddleware as AuthnMiddleware;
+use toubilib\api\middlewares\AuthzMiddleware as AuthzMiddleware;
 
 
 return function( \Slim\App $app):\Slim\App {
 
     $app->add(Cors::class);
 
-    $app->get('/', HomeAction::class);
-    $app->get('/praticiens', ListerPraticiensAction::class);
-    $app->get('/praticiens/{id}/rdvs', PraticienRDVAction::class);
-    $app->get('/rdvs/{id}', RendezVousByIdAction::class);
-    $app->post('/auth/signin', SignInAction::class);
-    $app->post('/auth/refresh', RefreshAction::class);
+    //$app->get('/', HomeAction::class);
+    $app->get('/praticiens', ListerPraticiensAction::class); //tester
+    //http://localhost:6080/praticiens/4305f5e9-be5a-4ccf-8792-7e07d7017363/rdvs?debut=2025-12-01&fin=2025-12-10
+    $app->get('/praticiens/{id}/rdvs', PraticienRDVAction::class); //tester
+    $app->get('/rdvs/{id}', RendezVousByIdAction::class); //tester
+    $app->post('/auth/signin', SignInAction::class); //tester
+    $app->post('/auth/refresh', RefreshAction::class); //tester
 
     $app->group('', function(\Slim\Routing\RouteCollectorProxy $group){
-        $group->get('/praticiens/{id}/agenda', ConsulterAgendaAction::class);
+      //http://localhost:6080/praticiens/4305f5e9-be5a-4ccf-8792-7e07d7017363/agenda?debut=2025-12-01&fin=2025-12-10
+        $group->get('/praticiens/{id}/agenda', ConsulterAgendaAction::class); //tester
         $group->post('/praticiens/{id}/rdvs', CreateRendezVousAction::class)
-        ->add(CreateRdvMiddleware::class);
-        $group->get('/praticiens/{id}', PraticienByIdAction::class);
-        $group->patch('/rdvs/{id}/annuler', AnnulerRendezVousAction::class);
+        ->add(CreateRdvMiddleware::class); //tester
+        $group->get('/praticiens/{id}', PraticienByIdAction::class); //tester
+        $group->patch('/rdvs/{id}/annuler', AnnulerRendezVousAction::class); //tester
     })
     ->add(AuthnMiddleware::class)
     ->add(AuthzMiddleware::class);
