a7da8e9937ab3cfc75761f2718bc452dcad93f28
Kinnig Felicien <felicien.kinnig9@etu.univ-lorraine.fr>
2025-12-05 11:07:49 +0100

    add(middleware): CreatePatient
    création drun middleware vérifiant les données et crée le dto à partir du body fournie

---
+++ toubilib/app/src/api/middlewares/CreatePatientMiddleware.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace toubilib\api\middlewares;
+
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Container\ContainerInterface;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Server\RequestHandlerInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use toubilib\core\application\ports\api\dtos\InputPatientDTO as InputPatientDTO;
+
+
+class CreatePatientMiddleware implements MiddlewareInterface{
+    public function process(ServerRequestInterface $rq, RequestHandlerInterface $rh) : Response{
+        $contentType = $rq->getHeaderLine('Content-Type');
+        $body = (String)$rq->getBody();
+
+        $data = [];
+        if(str_contains($contentType, 'application/json')){
+            $data = json_decode($body, true);
+            if($data === null){
+                return new Response(400, ['Content-Type' => 'application/json'], json_encode(['erreur' => 'JSON invalide']));
+            }
+        } else {
+            $data = $rq->getParsedBody() ?? [];
+        }
+
+        $erreurs = [];
+
+        $required = ['password' => 'string', 'nom' => 'string', 'prenom' => 'string', 'dateNaissance' => 'string', 'email' => 'string', 'telephone' => 'string'];
+        foreach($required as $r => $type){
+            if(!isset($data[$r])){
+                $erreurs[$r] = 'Champ requis';
+            }
+            $valeur = $data[$r];
+
+            switch($type){
+                case 'string':
+                    if (trim((String)$valeur) === ''){
+                        $erreurs[$r] = 'Contenu manquant';
+                    }
+                    break;
+                case 'int':
+                    if (!is_numeric($valeur)){
+                        $erreurs[$r] = 'Pas un Entier';
+                    }
+                    break;
+                case 'datetime':
+                    if(!$valeur instanceof \DateTimeImmutable){
+                        try{
+                            new \DateTimeImmutable($valeur);
+                        } catch(\Exception $e){
+                            $erreurs[$r] = 'Date Invalide';
+                        }
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+        
+        if(!empty($erreurs)){
+            throw new Error($erreurs);
+        }
+
+        $dto = new InputPatientDTO($data);
+        $rq = $rq->withAttribute('inputPatient', $dto);
+        return $rh->handle($rq);
+    }
+}
