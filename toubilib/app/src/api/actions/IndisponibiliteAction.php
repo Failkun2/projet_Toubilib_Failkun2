4ab554a1baa38138233de358d3dcb7f6a2f9f797
Kinnig Felicien <felicien.kinnig9@etu.univ-lorraine.fr>
2025-12-06 09:49:29 +0100

    update(action): Indisponibilite
    ajout de IndisponibiliteAction

---
+++ toubilib/app/src/api/actions/IndisponibiliteAction.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace toubilib\api\actions;
+
+use Psr\Container\ContainerInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use toubilib\api\actions\AbstractAction as AbstractAction;
+use toubilib\core\application\ports\ServiceRendezVousInterface as ServiceRendezVousInterface;
+
+class IndisponibiliteAction extends AbstractAction{
+
+    private ServiceRendezVousInterface $service;
+
+    public function __construct(ServiceRendezVousInterface $service){
+        $this->service = $service;
+    }
+
+    public function __invoke(ServerRequestInterface $rq, ResponseInterface $rs, array $args) : ResponseInterface{
+        $praticienId = $args['id'] ?? null;
+        $query = $rq->getQueryParams();
+        if(!$praticienId || !isset($query['debut']) || !isset($query['fin'])){
+            $json = json_encode(['erreur' => 'parametre manquant'], JSON_PRETTY_PRINT);
+            $rs->getBody()->write($json);
+            return $rs->withHeader('Content-type', 'application/json')->withStatus(400);
+        }
+        try {
+            $debut = new \DateTimeImmutable($query['debut']);
+            $fin = new \DateTimeImmutable($query['fin']);
+        } catch(\Exception $e){
+            $json = json_encode(['erreur' => 'dates invalides'], JSON_PRETTY_PRINT);
+            $rs->getBody()->write($json);
+            return $rs->withHeader('Content-type', 'application/json')->withStatus(400);
+        }
+        $newId = $this->service->creerIndisponibilite($praticienId, $debut, $fin);
+        $payload = [
+            'id' => $newId,
+            'message' => 'Indisponibilité créer'
+        ];
+        $rs->getBody()->write(json_encode($payload));
+        return $rs->withStatus(201)->withHeader('Content-Type', 'application/json');
+    }
+}
